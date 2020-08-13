e113856cf61ccea39e7f09fbb7b9b6d1f51f5812
Zeeshan Habib <zeshanhabib@icloud.com>
2020-08-13 13:20:46 +0500

    Add UiControllerView

--- Deals/ViewController.swift
+++ Deals/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var collectionImages: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+
+extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource
+{
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
 }
 
